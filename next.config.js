67ec184395fe471b52a7cc2a90ee08593309606d
nikkey78 <nick-bazh@yandex.ru>
2024-04-18 21:40:23 +0300

    Mutating Data

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        serverActions: {
+          allowedOrigins: ["website.com", "localhost:3000", "127.0.0.1:3000"]
+        }
+    }
+};
 
 module.exports = nextConfig;
